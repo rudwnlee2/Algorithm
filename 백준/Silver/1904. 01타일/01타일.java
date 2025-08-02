ad988901eaf617e1d278482577909e7e3c1b485c
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-08-02 15:02:21 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+
+		if(n == 1) {
+			System.out.println(1);
+			return;
+		}
+
+		if(n == 2) {
+			System.out.println(2);
+			return;
+		}
+
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+		}
+
+		System.out.println(dp[n]);
+	}
+}
