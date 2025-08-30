0e4285cee01d68944fef220c74e92da34b088808
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-08-30 14:41:28 +0900

    [level 2] Title: 주식가격, Time: 37.62 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = (n - 1) - index;
+        }
+
+        return answer;
+    }
+}
