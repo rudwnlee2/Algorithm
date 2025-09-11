22f9a2bd2b3507e5b07d18b5da5b4a2d1a0d3afb
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-09-11 22:26:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 164.88 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-
-        HashMap<String, HashSet<String>> map = new HashMap<>();
+        
         HashMap<String, Integer> index = new HashMap<>();
+        HashMap<String, HashSet> reportsCount = new HashMap<>();
         
         for(int i = 0; i < id_list.length; i++) {
-            HashSet<String> set = new HashSet<>();
-            map.put(id_list[i], set);
             index.put(id_list[i], i);
+            reportsCount.put(id_list[i], new HashSet());
         }
         
         for(int i = 0; i < report.length; i++) {
             String[] temp = report[i].split(" ");
-            map.get(temp[1]).add(temp[0]);
+            reportsCount.get(temp[1]).add(temp[0]);
         }
         
         int[] answer = new int[id_list.length];
         for(int i = 0; i < id_list.length; i++) {
-            HashSet<String> temp = map.get(id_list[i]);
-            if(temp.size() >= k) {
-                for (String s : temp) {
+            HashSet<String> set = reportsCount.get(id_list[i]);
+            if(set.size() >= k) {
+                for (String s : set) {
                     answer[index.get(s)]++;
                 }
             }
