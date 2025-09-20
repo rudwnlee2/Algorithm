d0c6b34044a1adc274bbb60e383821c8027c7b86
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-09-20 16:19:47 +0900

    [level 2] Title: 배달, Time: 3.72 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 class Solution {
     
-    static class Node implements Comparable<Node> {
+    static class Node implements Comparable<Node>{
+        
         int vertex;
         int weight;
-
+        
         public Node(int vertex, int weight) {
             this.vertex = vertex;
             this.weight = weight;
@@ -15,35 +16,37 @@ class Solution {
         public int compareTo(Node other) {
             return Integer.compare(this.weight, other.weight);
         }
+        
     }
     
     public int solution(int N, int[][] road, int K) {
+        int answer = 0;
         
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
+        List<ArrayList<Node>> graph = new ArrayList<>();
+        for(int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
         
         for(int i = 0; i < road.length; i++) {
             int start = road[i][0];
             int end = road[i][1];
-            int weight = road[i][2];
+            int w = road[i][2];
             
-            graph.get(start).add(new Node(end, weight));
-            graph.get(end).add(new Node(start, weight));
+            graph.get(start).add(new Node(end, w));
+            graph.get(end).add(new Node(start, w));
         }
         
-        int[] dist = new int[N+1];
+        int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
-        
+
         PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(1, 0));
+        q.offer(new Node(1, 0));
         
         while(!q.isEmpty()) {
-            Node now = q.remove();
+            Node now = q.poll();
             
-            if (now.weight > dist[now.vertex]) continue;
+            if(now.weight > dist[now.vertex]) continue;
             
             for(Node next : graph.get(now.vertex)) {
                 int cost = next.weight + dist[now.vertex];
@@ -55,10 +58,10 @@ class Solution {
             }
         }
         
-        int answer = 0;
         for (int i = 1; i <= N; i++) {
             if (dist[i] <= K) answer++;
         }
+        
         return answer;
     }
 }
