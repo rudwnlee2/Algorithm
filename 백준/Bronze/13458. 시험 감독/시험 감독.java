d103b9c1084f213fd9972bd56b0340c0ea5a79d9
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-10-18 16:18:29 +0900

    [Bronze II] Title: 시험 감독, Time: 436 ms, Memory: 92188 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		long result = n;
+		for(int i = 0; i < n; i++) {
+			a[i] -= b;
+
+			if(a[i] > 0) {
+				result += a[i] / c;
+				if(a[i] % c != 0) result++;
+			}
+			
+		}
+
+		System.out.println(result);
+		
+	}
+}
