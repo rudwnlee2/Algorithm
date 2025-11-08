625f442ebedabc858f81030607cd1915813ab7eb
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-08 15:40:37 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,47 +1,37 @@
 import java.util.*;
 
 class Solution {
+    
+    static boolean[] visited;
+    static int answer = 0;
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        boolean[] visited = new boolean[words.length];
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
         
-        Queue<String[]> queue = new LinkedList<>();
-        queue.offer(new String[] {begin, "0"});
+        return answer;
+    }
+    
+    private static void dfs(String begin, String target, String[] words, int count) {
+        if (begin.equals(target)) {
+            answer = count;
+            return;
+        }
         
-        while(!queue.isEmpty()) {
-            String[] temp = queue.poll();
-            String word = temp[0];
+        for (int i = 0; i < words.length; i++) {
+            if(visited[i]) continue;
             
-            if(target.equals(word)) {
-                return Integer.parseInt(temp[1]);
-            }
-            
-            int count = Integer.parseInt(temp[1]);
-            
-            for (int i = 0; i < word.length(); i++) {
-                char[] charWord = word.toCharArray();
+            int x = 0;
+            for(int j = 0; j < begin.length(); j++) {
+                if(begin.charAt(j) == words[i].charAt(j)) x++;
                 
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (charWord[i] == c) {
-                        continue; 
-                    }
-
-                    charWord[i] = c;
-                    String nextWord = new String(charWord);
-                    
-                    for (int j = 0; j < words.length; j++) {                    
-                        if (words[j].equals(nextWord) && !visited[j]) {
-                            queue.offer(new String[] {nextWord, String.valueOf(count + 1)});
-                            visited[j] = true;
-                            
-                            break; 
-                        }
-                    }
+                if(x == begin.length() - 1) {
+                    visited[i] = true;
+                    dfs(words[i], target, words, count + 1);
+                    visited[i] = false;
                 }
             }
             
         }
-        
-        return answer;
     }
 }
