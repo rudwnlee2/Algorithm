faea82065389dcd13b495bb5cd72c902dfef9020
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-11 16:42:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.17 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,56 @@
 import java.util.*;
 
 class Solution {
+    
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int n;
+    static int m;
+    
+    static boolean[][] visited;
+    
     public int solution(int[][] maps) {
+        int answer = 0;
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
         
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        answer = bfs(maps);
+        
+        return answer;
+    }
+    
+    private static int bfs(int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        int[] rx ={0, 0, -1, 1};
-        int[] ry ={-1, 1, 0, 0};
-        
         q.offer(new int[] {0, 0, 1});
         visited[0][0] = true;
         
         while(!q.isEmpty()) {
             int[] temp = q.poll();
-            int y = temp[0];
-            int x = temp[1];
-            int count = temp[2];
             
-            if (y == maps.length - 1 && x == maps[0].length - 1) {
-                return count;
+            if(temp[0] == n - 1 && temp[1] == m - 1) {
+                return temp[2];
             }
             
             for(int i = 0; i < 4; i++) {
-                int ny = y + ry[i];
-                int nx = x + rx[i];
+                int y = temp[0] + dy[i];
+                int x = temp[1] + dx[i];
+                int count = temp[2];
                 
-                if(ny < 0 || nx < 0 || ny >= maps.length || nx >= maps[0].length) {
+                if(y < 0 || x < 0 || y >= n || x >= m) {
                     continue;
                 }
                 
-                if(visited[ny][nx]) {
+                if(visited[y][x]) {
                     continue;
                 }
                 
-                if(maps[ny][nx] == 1) {
-                    q.offer(new int[] {ny, nx, count + 1});
-                    visited[ny][nx] = true;
+                if(maps[y][x] == 1) {
+                    visited[y][x] = true;
+                    q.offer(new int[] {y, x, count + 1});
                 }
+                
             }
-            
         }
         
         return -1;
