04abb0b83f0bfd306ef629f7c40fdf382722f4e8
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-20 14:45:13 +0900

    [D3] Title: 농작물 수확하기, Time: 92 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,48 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int result;
-    
 	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int t = Integer.parseInt(br.readLine());
-        
-		for (int tc = 1; tc <= t; tc++) {
-			result = 0;
-            solution();
-			System.out.println("#" + tc + " " + result);
+		for(int test_case = 1; test_case <= t; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+			int[][] arr = new int[n][n];
+			
+			for(int i = 0; i < n; i++) {
+				String temp = br.readLine();
+				for(int j = 0; j < n; j++) {
+					arr[i][j] = temp.charAt(j) - '0';
+				}
+			}
+			
+			int count = 0;
+			int start = n / 2;
+			int end = n / 2;
+			for(int i = 0; i < n / 2 + 1; i++) {
+				for(int j = start; j <= end; j++) {
+					count += arr[i][j];
+					if (i != n / 2) {
+						count += arr[n - 1 - i][j];
+					}
+				}
+				start--;
+				end++;
+			}
+			
+			System.out.println("#" + test_case + " " + count);
 		}
-        
+		
 	}
-    
-    static void solution() throws Exception {
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            String temp = br.readLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j]=temp.charAt(j) - '0';
-            }
-        }
-        
-        int start = n / 2;
-        int end = n / 2;
-        for (int i = 0; i < n; i++) {
-            for (int j = start; j <= end; j++) {
-                result += arr[i][j];
-            }
-            
-            if (i < n / 2) {
-                start -= 1;
-                end += 1;
-            } else {
-                start += 1;
-                end -= 1;
-            }
-        }
-        
-    }
 }
