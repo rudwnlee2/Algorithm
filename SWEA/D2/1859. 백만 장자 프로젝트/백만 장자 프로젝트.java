3a853b952da18c7b3984a11e51d81684150d8371
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-20 14:14:10 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 361 ms, Memory: 139,736 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,40 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
 	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
 		
-        int t = Integer.parseInt(br.readLine());
-
-		for(int ts = 1; ts <= t; ts++) {
-			long result = solution();
-			System.out.println("#" + ts + " " + result);
+		for(int test_case = 1; test_case <= t; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			long count = 0;
+			int max = 0;
+			for(int i = n - 1; i >= 0; i--) {
+				if(max < arr[i]) {
+					max = arr[i];
+				} else {
+					count += max - arr[i];
+				}
+			}
+			
+			System.out.println("#" + test_case + " " + count);
 		}
 	}
-    
-    private static long solution() throws Exception {
-        long count = 0;
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int max = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            if(max < arr[i]) {
-                max = arr[i];
-            } else {
-                count += max - arr[i];
-            }
-        }
-        
-        return count;
-    }
 }
