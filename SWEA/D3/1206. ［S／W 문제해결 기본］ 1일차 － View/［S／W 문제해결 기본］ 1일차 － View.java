a6fa4d46390405b8071a1b3630b2ba46dffad0af
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-20 13:51:52 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 99 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,35 +1,34 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
 	public static void main(String args[]) throws Exception {
-		for(int tc = 1; tc <= 10; tc++) {
-            int result = solution();
-			System.out.println("#" + tc + " " + result);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for(int ts = 1; ts<= 10; ts++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int count = 0;
+			for(int i = 2; i < n - 2; i++) {
+				int leftMax = Math.max(arr[i - 1], arr[i - 2]);
+				int rightMax = Math.max(arr[i + 1], arr[i + 2]);
+				
+				int max = Math.max(leftMax, rightMax);
+				
+				if(arr[i] > max) {
+					count += arr[i] - max;
+				}
+			}
+			
+			System.out.println("#" + ts + " " + count);
 		}
 	}
-    
-    static int solution() throws Exception {
-        int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int total = 0;
-        for(int i = 2; i < n - 2; i++) {
-            int left = Math.max(arr[i - 1], arr[i - 2]);
-            int rifgt = Math.max(arr[i + 1], arr[i + 2]);
-            int max = Math.max(left, rifgt);
-            
-            if(arr[i] > max) total += arr[i] - max;
-        }
-        
-        return total;
-  
-    }
 }
