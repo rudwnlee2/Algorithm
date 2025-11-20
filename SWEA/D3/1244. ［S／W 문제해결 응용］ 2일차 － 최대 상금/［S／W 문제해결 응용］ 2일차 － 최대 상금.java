16bde6cc3187d24283f0a021f4405216638931e9
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-20 15:59:06 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 3,446 ms, Memory: 112,384 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,62 +1,53 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    static char arr[];
-    static int n;
-    static String result;
-    
+	
+	static String max;
+	static char[] arr;
+	static int count;
+	
 	public static void main(String args[]) throws Exception {
-        
-        int T = Integer.parseInt(br.readLine());
-
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int answer = 0;
-            answer = solutuion();
-			System.out.println("#" + test_case + " " + answer);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int ts = 1; ts <= t; ts++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String s = st.nextToken();
+			count = Integer.parseInt(st.nextToken());
+			
+			arr = s.toCharArray();
+			max = "0";
+			
+			dfs(0, 0);
+			System.out.println("#" + ts + " " + max);
 		}
 	}
-    
-    static int solutuion() throws Exception {
-        
-        StringTokenizer st =new StringTokenizer(br.readLine());
-        String s = st.nextToken();
-        n = Integer.parseInt(st.nextToken());
-        arr = s.toCharArray();
-        result = "";
-        
-        dfs(0, 0);
-        
-        return Integer.parseInt(result);
-    }
-    
-    static void dfs(int depth, int count) {
-        
-        if(count == n){
-            String current = new String(arr); 
-            if (current.compareTo(result) > 0) {  
-                result = current;  
-            }
-            return;
-       }
-        
-        for(int i = depth; i < arr.length - 1; i++) {
-           for(int j = i + 1; j < arr.length; j++) {
-               change(i, j);
-               dfs(i, count + 1);
-               change(i, j);
-           }
-       }
-        
-    }
-    
-    static void change(int i, int j){
+	
+	private static void dfs(int index, int change) {
+		
+		if(change == count) {
+			String s = new String(arr);
+			if(s.compareTo(max) > 0) {
+				max = s;
+			}
+			return;
+		}
+		
+		for(int i = index; i < arr.length - 1; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				swap(i, j);
+				dfs(i, change + 1);
+				swap(i, j);
+			}
+		}
+	}
+	
+	static void swap(int i, int j){
         char temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    
 }
