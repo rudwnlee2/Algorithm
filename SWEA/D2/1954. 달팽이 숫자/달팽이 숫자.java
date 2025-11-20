0b1970cf34cab3e57cf9a9420e76d05b53c67a21
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-20 10:07:43 +0900

    [D2] Title: 달팽이 숫자, Time: 85 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,56 +1,48 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
 	public static void main(String args[]) throws Exception {
-        
-        int t = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
-		for(int ts = 1; ts <= t; ts++) {
-			int[][] result = solution();
-			System.out.println("#" + ts);
-            for(int[] temp : result) {
-                for(int num : temp) {
-                    System.out.print(num + " ");
-                }
-                System.out.println();
-            }
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+			int[][] arr = new int[n][n];
+			
+			int[] dy = {0, 1, 0, -1};
+			int[] dx = {1, 0, -1, 0};
+			int dir = 0;
+			int num = 1;
+			int y = 0;
+			int x = 0;
+			
+			while(num <= n * n) {
+				arr[y][x] = num;
+				
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				
+				if(ny < 0 || ny >= n || nx < 0 || nx >= n || arr[ny][nx] != 0) {
+	                dir = (dir + 1) % 4;
+	                ny = y + dy[dir];
+	                nx = x + dx[dir];
+	            }
+				
+				y = ny;
+				x = nx;
+				
+				num++;
+			}
+			
+			System.out.println("#" + test_case);
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
 		}
 	}
-    
-    private static int[][] solution() throws Exception {
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
-        
-        int[] dy = { 0, 1, 0, -1 }; 
-		int[] dx = { 1, 0, -1, 0 };
-		int y = 0;
-		int x = 0;
-        
-        int dir = 0;
-        int num = 1;
-        
-        while(num <= n * n) {
-            arr[y][x] = num;
-            
-            int ny = y + dy[dir];
-            int nx = x + dx[dir];
-            
-            if(ny < 0 || ny >= n || nx < 0 || nx >= n || arr[ny][nx] != 0) {
-                dir = (dir + 1) % 4;
-                ny = y + dy[dir];
-                nx = x + dx[dir];
-            }
-            
-            y = ny;
-            x = nx;
-            num += 1;
-        }
-        
-        return arr;
-    }
-    
 }
