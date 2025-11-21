982587215e2a768041345be46d75f5f7383a7beb
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-21 17:13:29 +0900

    [D2] Title: 숫자 배열 회전, Time: 118 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -2,57 +2,48 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int n;
-    static int[][] arr90;
-    static int[][] arr180;
-    static int[][] arr270;
-    
 	public static void main(String args[]) throws Exception {
-		int t = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
 
-		for(int ts= 1; ts <= t; ts++) {
-            solution();
-            System.out.println("#" + ts);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(arr90[i][j]);
+		for(int ts = 1; ts <= t; ts++) {
+			int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n][n];
+			
+            for(int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
-                System.out.print(" ");
-
-                for (int j = 0; j < n; j++) {
-                    System.out.print(arr180[i][j]);
-                }
-                System.out.print(" ");
-
-				for (int j = 0; j < n; j++) {
-                    System.out.print(arr270[i][j]);
-                }
-                System.out.println();
-
             }
-        }
+            
+			int[][] arr90 = change(arr);
+			int[][] arr180 = change(arr90);
+			int[][] arr270 = change(arr180);
+			
+			System.out.println("#" + ts);
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.print(arr90[i][j]);
+				}
+				System.out.print(" ");
+				
+				for(int j = 0; j < n; j++) {
+					System.out.print(arr180[i][j]);
+				}
+				System.out.print(" ");
+				
+				for(int j = 0; j < n; j++) {
+					System.out.print(arr270[i][j]);
+				}
+				System.out.println();
+			}
+		}
 	}
     
-    private static void solution() throws Exception {
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        
-        int[][] arr = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        arr90 = rotation(arr);
-        arr180 = rotation(arr90);
-        arr270 = rotation(arr180);
-    }
-    
-    private static int[][] rotation(int[][] arr) {
+    private static int[][] change(int[][] arr) {
         int n = arr.length;
         int[][] result = new int[n][n];
         
@@ -61,7 +52,7 @@ class Solution {
                 result[i][j] = arr[n - 1 - j][i];
             }
         }
+        
         return result;
     }
-        
 }
