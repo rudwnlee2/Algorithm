a93ab55aeed628fa3151d23de2c89bc1f64de76d
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-22 12:06:51 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 215 ms, Memory: 36,332 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,51 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Solution {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
 	public static void main(String args[]) throws Exception {
-
-		for(int tc = 1; tc <= 10; tc++) {
-            int t = Integer.parseInt(br.readLine());
-            int result = solution();
-			System.out.println("#" + tc + " " + result);
-		}
-	}
-    
-    
-    static int solution() throws Exception {
-        int[][] arr = new int[100][100];
-        StringTokenizer st;
-        int cy = 0;
-        int cx = 0;
-        
-        for (int i = 0; i < 100; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 100; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if (arr[i][j] == 2) {
-                    cy = i;
-                    cx = j;
-                }
-            }
-        }
-
-		int way = 3;
-		while (cy > 0) { 
-			if (cx - 1 >= 0 && arr[cy][cx - 1] == 1 && way != 2) {
-				cx--;
-				way = 1;
-			} else if (cx + 1 < 100 && arr[cy][cx + 1] == 1 && way != 1) {
-				cx++;
-				way = 2;
-			} else {
-				cy--;
-				way = 3;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		for(int ts = 1; ts <= 10; ts++) {
+			int t = Integer.parseInt(br.readLine());
+			int startY = 0;
+			int startX = 0;
+			
+			int[][] arr = new int[100][100];
+			for(int i = 0; i < 100; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < 100; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+					if(arr[i][j] == 2) {
+						startY = i;
+						startX = j;
+					}
+				}
 			}
+			
+			int result = 0;
+			int dir = 0;
+			Queue<int[]> q = new LinkedList<>();
+			q.offer(new int[] {startY, startX, dir});
+			
+			while(!q.isEmpty()) {
+				int[] temp = q.poll();
+				int y = temp[0];
+				int x = temp[1];
+				dir = temp[2];
+				
+				if(y == 0) {
+					result = x;
+					break;
+				}
+				
+				if (x - 1 >= 0 && arr[y][x - 1] == 1 && dir != 2) {
+					q.offer(new int[] {y, x - 1, 1});
+				} else if (x + 1 < 100 && arr[y][x + 1] == 1 && dir != 1) {
+					q.offer(new int[] {y, x + 1, 2});
+				} else {
+					q.offer(new int[] {y - 1, x, 3});
+				}
+				
+			}
+			
+			System.out.println("#" + t + " " + result);
 		}
-
-	return cx;
 	}
 }
