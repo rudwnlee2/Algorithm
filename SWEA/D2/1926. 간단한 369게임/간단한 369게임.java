405886231c96aa37018b58fc154e020731c9c029
rudwnlee2 <155804041+rudwnlee2@users.noreply.github.com>
2025-11-22 12:15:30 +0900

    [D2] Title: 간단한 369게임, Time: 76 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,18 +1,17 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Solution {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        for(int i = 1; i <= n; i++) {
-            String temp = String.valueOf(i);
-            
-            if (temp.contains("3") || temp.contains("6") || temp.contains("9")) {
-                for (int j = 0; j < temp.length(); j++) {
-                    if (temp.charAt(j) == '3' || temp.charAt(j) == '6' || temp.charAt(j) == '9') {
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			String s = String.valueOf(i);
+			
+			if (s.contains("3") || s.contains("6") || s.contains("9")) {
+                for (int j = 0; j < s.length(); j++) {
+                    if (s.charAt(j) == '3' || s.charAt(j) == '6' || s.charAt(j) == '9') {
                         System.out.print("-");
                     }
                     
@@ -21,8 +20,8 @@ class Solution {
             } else {
                 System.out.print(i + " ");
             }
-            
-        }
-        
+			
+		}
+		
 	}
 }
