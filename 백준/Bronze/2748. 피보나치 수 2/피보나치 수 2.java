007597280239876a94a566b7214927bdd4b7a0b7
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2025-12-19 22:29:34 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		if(n == 1) {
+			System.out.println(1);
+			return;
+		}
+		
+		long[] dp = new long[n + 1];
+		dp[1] = 1;
+		dp[2] = 1;
+		for(int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 2] + dp[i - 1];
+		}
+		System.out.println(dp[n]);
+	}
+}
