8e58863ad88f82779c9f992c4556c8cc63f7ecad
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2025-12-31 18:50:11 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 444 ms, Memory: 70768 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Main {
+
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		sb.append((int) (Math.pow(2, n) - 1)).append('\n');
+		hanoi(n, 1, 2, 3);
+		System.out.println(sb);
+	}
+
+	public static void hanoi(int n, int start, int mid, int to) {
+		if(n == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
+
+		hanoi(n - 1, start, to, mid);
+		sb.append(start + " " + to + "\n");
+		hanoi(n - 1, mid, start, to);
+	}
+}
