20f51318b63e1e583aec659423f42c8785a90697
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-01-04 13:17:01 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 17124 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	static char[][] arr;
+	static int n;
+	static int[] my = {-1, 1, 0, 0};
+	static int[] mx = {0, 0, -1, 1};
+	static boolean[][] visited;
+	static char temp;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+
+		arr = new char[n][n];
+		visited = new boolean[n][n];
+		for(int i = 0; i < n; i++) {
+			String s = br.readLine();
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = s.charAt(j);
+			}
+		}
+
+		int count1 = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(!visited[i][j]) {
+					temp = arr[i][j];
+					dfs1(i, j);
+					count1 += 1;
+				}
+			}
+		}
+
+		visited = new boolean[n][n];
+		int count2 = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(!visited[i][j]) {
+					temp = arr[i][j];
+					dfs2(i, j);
+					count2 += 1;
+				}
+			}
+		}
+		
+		System.out.println(count1 + " " + count2);
+	}
+
+	private static void dfs1(int y, int x) {
+		for(int i = 0; i < 4; i++) {
+			int ny = y + my[i];
+			int nx = x + mx[i];
+
+			if(ny >= n || nx >= n || ny < 0 || nx < 0) continue;
+			if(visited[ny][nx]) continue;
+			
+			if(temp == arr[ny][nx]) {
+				visited[ny][nx] = true;
+				dfs1(ny, nx);
+			}
+		}
+	}
+
+	private static void dfs2(int y, int x) {
+		for(int i = 0; i < 4; i++) {
+			int ny = y + my[i];
+			int nx = x + mx[i];
+
+			if(ny >= n || nx >= n || ny < 0 || nx < 0) continue;
+			if(visited[ny][nx]) continue;
+			
+			if(temp == 'R' || temp == 'G') {
+				if(arr[ny][nx] == 'R' || arr[ny][nx] == 'G') {
+					visited[ny][nx] = true;
+					dfs2(ny, nx);
+				}
+			} else {
+				if(temp == arr[ny][nx]) {
+					visited[ny][nx] = true;
+					dfs2(ny, nx);
+				}
+			}
+		}
+	}
+	
+}
