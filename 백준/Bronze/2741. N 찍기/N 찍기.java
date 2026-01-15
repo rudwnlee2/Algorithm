734675c1633ad8ca0a0e98bc2e0b5639c84bff6c
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-01-15 19:14:20 +0900

    [Bronze V] Title: N 찍기, Time: 840 ms, Memory: 31876 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		in.close();
+        
+		int i = 1;
+        
+		while(i <= n) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
