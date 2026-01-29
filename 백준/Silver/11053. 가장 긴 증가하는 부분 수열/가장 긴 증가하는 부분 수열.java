1381ca74651bd45793eb1dd4815d031aa03119bb
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-01-29 15:24:10 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,31 +5,28 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr1 = new int[n];
-
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
-			arr1[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int[] arr2 = new int[n];
-		int index = 0;
-		
 		for(int i = 0; i < n; i++) {
-			if(index == 0 || arr1[i] > arr2[index - 1]) {
-				arr2[index] = arr1[i];
-				index += 1;
-			} else{
-				int pos = Arrays.binarySearch(arr2, 0, index, arr1[i]);
-				if (pos < 0) {
-					pos = -(pos + 1);
+			dp[i] = 1;
+			for(int j = 0; j < i; j++) {
+				if(arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[j] + 1, dp[i]);
 				}
-				arr2[pos] = arr1[i];
 			}
-
+		}
+		
+		int max = 0;
+		for(int i = 0; i < n; i++) {
+			max = Math.max(max, dp[i]);
 		}
 
-		System.out.println(index);
-		
+		System.out.println(max);
 	}
 }
