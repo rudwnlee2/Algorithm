b62121bbaf2e2e54758ff739eb29843f1a614659
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-02-27 14:53:46 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -2,71 +2,58 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-	static boolean[] visited = new boolean[101];
-	static Map<Integer, Integer> moveMap = new HashMap<>();
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken()); 
+
+		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
+		int[][] board = new int[101][1];
+		
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			moveMap.put(start, end);
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			board[x][0] = y;
 		}
-
+		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			moveMap.put(start, end);
-			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			board[x][0] = y;
 		}
-		
-		int result = bfs();
-		System.out.println(result);
-	}
 
-	static int bfs() {
-
-		Queue<int[]> queue = new LinkedList<int[]>();
-		queue.offer(new int[] {1, 0});
-
+		boolean[] visited = new boolean[101];
 		visited[1] = true;
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {1, 0});
 
-		while(!queue.isEmpty()){
-			int[] now = queue.poll();
-			int pos = now[0];
-			int count = now[1];
+		int min = 0;
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
 
-			if (pos == 100) return count;
+			if(temp[0] == 100) {
+				min = temp[1];
+				break;
+			}
 
 			for(int i = 1; i <= 6; i++) {
-				int next = pos + i;
-
-				if (next > 100) continue;
-				if (visited[next]) continue;
+				int next = temp[0] + i;
 				
-				if (moveMap.containsKey(next)) {
-					next = moveMap.get(next);
-        }
+				if(next > 100 || visited[next]) continue;
 
-				if (!visited[next]) {
-          visited[next] = true;
-          queue.offer(new int[] {next, count + 1});
-        }
-				
+				if(board[next][0] != 0) {
+					q.offer(new int[] {board[next][0], temp[1] + 1});
+					visited[next] = true;
+				} else {
+					q.offer(new int[] {next, temp[1] + 1});
+					visited[next] = true;
+				}
 			}
-			
 		}
 
-		return -1;
-		
+		System.out.println(min);
 	}
 }
