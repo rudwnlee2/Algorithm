8ddb710e7091528b77f7e161c3b280693383ac8e
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-04-22 19:13:56 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,78 +1,82 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Main {
-	
-	static int n;
-	static int m;
-	static int[][] arr;
-	static int result;
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, 1, 0, -1};
-
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
 		st = new StringTokenizer(br.readLine());
-		int y = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		//0 북, 1 동, 2 남, 3서
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		int dir = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[n][m];
 		
-		arr = new int[n][m];
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < m; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		result = 0;
-		dfs(y, x, dir);
-		System.out.println(result);
-	}
-	
-	private static void dfs(int y, int x, int dir) {
-		if(arr[y][x] == 0) {
-			arr[y][x] = 2;
-			result += 1;
-		}
-		
-		for(int i = 0; i < 4; i++) {
+
+		int count = 0;
+
+		int[] dy = {-1, 0, 1, 0};
+		int[] dx = {0, 1, 0, -1};
+
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {r, c});
+
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
+			int y = temp[0];
+			int x = temp[1];
+
+			if(arr[y][x] == 0) {
+				arr[y][x] = 2;
+				count++;
+			}
 			
-			dir = (dir + 3) % 4;
-			
-			int ny = y + dy[dir];
-			int nx = x + dx[dir];
-			
-			if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-			
-			if(arr[ny][nx] == 2) {
-				continue;
-			} else {
+			boolean check = false;
+			for(int i = 0; i < 4; i++) {
+
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+
+				if(ny >= n || nx >= m || ny < 0 || nx < 0) continue;
+
 				if(arr[ny][nx] == 0) {
-					dfs(ny, nx, dir);
-					return;
+					check = true;
+					break;
+				}
+			}
+
+			if(!check) {
+				int ny = y - dy[dir];
+				int nx = x - dx[dir];
+
+				if(ny >= n || nx >= m || ny < 0 || nx < 0 || arr[ny][nx] == 1) break;
+				
+				q.offer(new int[] {ny, nx});
+			} else {
+				dir = (dir + 3) % 4;
+
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				
+				if(arr[ny][nx] == 0) {
+					q.offer(new int[] {ny, nx});
+				} else {
+					q.offer(new int[] {y, x});
 				}
 			}
 			
 		}
 		
-		int backDir = (dir + 2) % 4;
-		int by = y + dy[backDir];
-		int bx = x + dx[backDir];
-		
-		if(by < 0 || bx < 0 || by >= n || bx >= m) return;;
-		
-		if(arr[by][bx] != 1) {
-			dfs(by, bx, dir);
-		} 
-		
+		System.out.println(count);
 	}
-	
 }
