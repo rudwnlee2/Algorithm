a3c5e9f5255910032afbc1a89c939b297f1507c8
GyeongJu Lee <155804041+rudwnlee2@users.noreply.github.com>
2026-05-18 15:33:36 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 12.28 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        int n = board.length;
+        int[][] arr = new int[n][n];
+        boolean[][][] visited = new boolean[n][n][4];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        
+        int[] dy = {1, 0, -1, 0};
+        int[] dx = {0, 1, 0, -1};
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[3] - o2[3]);
+        
+        pq.offer(new int[] {0, 0, 0, 0});
+        pq.offer(new int[] {0, 0, 1, 0});
+        
+        while(!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            int y = temp[0];
+            int x = temp[1];
+            int dir = temp[2];
+            int cost = temp[3];
+            
+            if(y == n - 1 && x == n - 1) {
+                answer = cost;
+                break;
+            }
+            
+            visited[y][x][dir] = true;
+            
+            arr[y][x] = Math.min(arr[y][x], cost);
+            
+            for(int i = 0; i < 4; i++) {
+                int nd = (dir + i) % 4;
+                int ny = y + dy[nd];
+                int nx = x + dx[nd];
+            
+                if(ny >= n || nx >= n || ny < 0 || nx < 0) continue;
+                if(board[ny][nx] == 1 || visited[ny][nx][nd]) continue;
+                
+                if(nd == dir) {
+                    pq.offer(new int[] {ny, nx, nd, cost + 100});
+                } else {
+                    pq.offer(new int[] {ny, nx, nd, cost + 500 + 100});
+                }
+                
+            }
+            
+        }
+        
+        return answer;
+    }
+}
